ac56cbc5af4dfb223071ab4c33fddc3fcb0e9f98
buboegbu <buboegbu@NUCA-19220>
2016-06-07 21:02:05 +0300

    Initial commit

--- StrongNameKeyGenerator/SnCommand.cs
+++ StrongNameKeyGenerator/SnCommand.cs
@@ -201,55 +201,6 @@ namespace StrongNameKeyGenerator
             return ret;
         }
 
-        //public static ProjectItem FindSolutionItemByName(DTE dte, string name, bool recursive)
-        //{
-        //    ProjectItem projectItem = null;
-        //    foreach (Project project in dte.Solution.Projects)
-        //    {
-        //        projectItem = FindProjectItemInProject(project, name, recursive);
-
-        //        if (projectItem != null)
-        //        {
-        //            break;
-        //        }
-        //    }
-        //    return projectItem;
-        //}
-
-        //public static ProjectItem FindProjectItemInProject(Project project, string name, bool recursive)
-        //{
-        //    ProjectItem projectItem = null;
-
-        //    if (project.Kind != EnvDTE.Constants.vsProjectKindSolutionItems)
-        //    {
-        //        if (project.ProjectItems != null && project.ProjectItems.Count > 0)
-        //        {
-        //            projectItem = DteHelper.FindItemByName(project.ProjectItems, name, recursive);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        // if solution folder, one of its ProjectItems might be a real project
-        //        foreach (ProjectItem item in project.ProjectItems)
-        //        {
-        //            Project realProject = item.Object as Project;
-
-        //            if (realProject != null)
-        //            {
-        //                projectItem = FindProjectItemInProject(realProject, name, recursive);
-
-        //                if (projectItem != null)
-        //                {
-        //                    break;
-        //                }
-        //            }
-        //        }
-        //    }
-
-        //    return projectItem;
-        //}
-
-
 
     }
 
